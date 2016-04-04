a82ac05348e4dc54557ac8a647ef77dcd73f95f0
Brian Foo <brian@youaremyjoy.org>
2016-04-04 17:30:26 -0400

    multiple speaker typo, pull collections from cache, only show collections in project

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -7,6 +7,12 @@ class Collection < ActiveRecord::Base
     uid
   end
 
+  def self.getForHomepage
+    Rails.cache.fetch("#{ENV['PROJECT_ID']}/collections", expires_in: 10.minutes) do
+      Collection.where(project_uid: ENV['PROJECT_ID']).order("title")
+    end
+  end
+
   def self.getForDownloadByVendor(vendor_uid, project_uid)
     vendor = Vendor.find_by_uid(vendor_uid)
     Collection.where("vendor_id = :vendor_id AND vendor_identifier != :empty AND project_uid = :project_uid",
