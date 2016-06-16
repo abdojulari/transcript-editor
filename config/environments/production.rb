f392e618be4b2fd62b9e7a216c07ca58ef6ed75a
Glen Crawford <glencraw4d@gmail.com>
2016-06-17 09:03:51 +1000

    PT#121636695 - Tweak config for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,17 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  unless ENV['MEMCACHIER_SERVERS'].nil? || ENV["MEMCACHIER_USERNAME"].nil? || ENV["MEMCACHIER_PASSWORD"].nil?
-    config.cache_store = :dalli_store,
-                        (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                        {:username => ENV["MEMCACHIER_USERNAME"],
-                         :password => ENV["MEMCACHIER_PASSWORD"],
-                         :failover => true,
-                         :socket_timeout => 1.5,
-                         :socket_failure_delay => 0.2
-                        }
-  end
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -33,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
