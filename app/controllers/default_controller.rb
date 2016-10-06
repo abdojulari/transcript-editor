29f350edad66b0ae59769ef3c51f3ccb9adcea65
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2016-10-06 13:17:21 +1100

    [AM-9] Use environment-specific index

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,8 +1,13 @@
 class DefaultController < ApplicationController
 
   def index
-    # render the project set in environment
-    render :file => "public/#{ENV['PROJECT_ID']}/index.html"
+    # Render the project set in environment.
+    # If there's a different index file available
+    # for the current Rails.env, use that instead.
+    index_file = "public/#{ENV['PROJECT_ID']}/index.html"
+    env_index_file = "public/#{ENV['PROJECT_ID']}/index.#{Rails.env.downcase}.html"
+    index_file = env_index_file if File.exists?(env_index_file)
+    render :file => index_file
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def css_class_rails_environment
-    "application-env-#{Rails.env.downcase}"
-  end
 end
