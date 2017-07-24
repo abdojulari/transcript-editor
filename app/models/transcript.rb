6bca9a1502f1a4c858c402c5c17e75d569c8bb93
Riana Ferreira <rianaferreira@gmail.com>
2017-07-25 08:51:37 +1000

    Handle speaker updating in a transaction

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -31,16 +31,18 @@ class Transcript < ActiveRecord::Base
   def speakers=(params)
     return unless valid?
 
-    # remove the exist transcript speakers
-    transcript_speakers.destroy_all
+    ActiveRecord::Base.transaction do
+      # remove the exist transcript speakers
+      transcript_speakers.destroy_all
 
-    # replace the transcript speakers with the new or revised selection
-    params.split(';').reject { |c| c.blank? }.each do |name|
-      transcript_speakers.build(
-        speaker_id: Speaker.find_or_create_by(name: name.strip).id,
-        collection_id: collection.id,
-        project_uid: project_uid
-      )
+      # replace the transcript speakers with the new or revised selection
+      params.split(';').reject(&:blank?).each do |name|
+        transcript_speakers.build(
+          speaker_id: Speaker.find_or_create_by(name: name.strip).id,
+          collection_id: collection.id,
+          project_uid: project_uid
+        )
+      end
     end
   end
 
