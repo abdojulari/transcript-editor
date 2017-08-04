188d620d9355719d63a201cbfe3f8ddf20f61b0c
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2017-08-04 15:07:04 +1000

    [AM-64] Eliminate azuread monkeypatch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,40 +2,6 @@
 
 require 'omniauth/saml/certificate_loader'
 
-module OmniAuth
-  module Strategies
-    # Monkey-patch for Omniauth Azure AD.
-    # @see https://github.com/AzureAD/omniauth-azure-activedirectory/issues/25
-    class AzureActiveDirectory
-      def raw_authorize_endpoint_url
-        hostname = 'login.windows.net'
-        if ENV.key?('AZUREAD_ENDPOINT_HOSTNAME')
-          hostname = ENV['AZUREAD_ENDPOINT_HOSTNAME']
-        end
-        'https://' + hostname + '/common/oauth2/authorize'
-      end
-
-      def authorize_endpoint_url
-        uri = URI(raw_authorize_endpoint_url)
-        uri.query = URI.encode_www_form(client_id: client_id,
-                                        redirect_uri: callback_url,
-                                        response_mode: response_mode,
-                                        response_type: response_type,
-                                        nonce: new_nonce)
-        uri.to_s
-      end
-
-      def verify_options
-        { verify_expiration: true,
-          verify_not_before: true,
-          verify_iat: true,
-          verify_aud: true,
-          'aud' => client_id }
-      end
-    end
-  end
-end
-
 # Initialise Omniauth config.
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Google OAuth2 authentication.
