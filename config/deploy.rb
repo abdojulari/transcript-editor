21026bb2d2cde4f23c61b4f62872ed3d4025ef22
sameera207 <sameera207@gmail.com>
2018-05-24 10:37:50 +1000

    more logs added

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,8 @@ set :rvm_ruby_version, 'ruby-2.5.0@rails5'
 p "--------------------------------------------------------------"
 p shared_path
 
+p "---------------1----------------------------------------------"
+
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
@@ -28,11 +30,15 @@ set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
+
+p "---------------2----------------------------------------------"
 set :puma_env, fetch(:rack_env, fetch(:rails_env))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
+
+p "---------------3----------------------------------------------"
 set :puma_preload_app, false
 
 p "--------------------------------------------------------------"
