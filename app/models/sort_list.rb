170884de2f17d09db720cd70b472f5c26e5a5744
sameera207 <sameera207@gmail.com>
2018-07-04 12:46:09 +1000

    collection option removed from the homepage sorting options

--- app/models/sort_list.rb
+++ app/models/sort_list.rb
@@ -10,9 +10,7 @@ class SortList
       percent_completed_desc: 'Completeness (most to least)',
       percent_completed_asc: 'Completeness (least to most)',
       duration_asc: 'Duration (short to long)',
-      duration_desc: 'Duration (long to short)',
-      collection_id_asc: 'Collection'
-
+      duration_desc: 'Duration (long to short)'
     }.each do |key, value|
       arr << OpenStruct.new(id: key, title: value)
     end
