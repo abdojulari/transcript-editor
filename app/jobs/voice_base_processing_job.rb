535656f05e5e54812d0c34ec463c5c2553b11b0b
sameera207 <sameera207@gmail.com>
2018-09-21 10:09:27 +1000

    scope updated to pickup the jobs

--- app/jobs/voice_base_processing_job.rb
+++ app/jobs/voice_base_processing_job.rb
@@ -2,7 +2,7 @@ class VoiceBaseProcessingJob < ApplicationJob
   queue_as :default
 
   def perform
-    Transcript.voicebase_processing_pending.not_picked_up_for_voicebase_processing.find_each do |transcript|
+    Transcript.voicebase_processing_pending.find_each do |transcript|
       VoiceBase::VoicebaseApiService.check_progress(transcript.id)
       transcript = Transcript.find(transcript.id)
       VoiceBase::VoicebaseApiService.process_transcript(transcript.id)
