d9d0ddbdb6c84e70f177e8ac9666a1840eebb56b
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2018-09-24 13:51:02 +1000

    Changed bundle path for prod

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,8 +6,13 @@ env :PATH, ENV['PATH']
 current_env = ENV['RAILS_ENV'] || 'development'
 deployed = ['staging', 'production'].include?(current_env)
 
+path_to_bundle = '/home/deploy/.rvm/wrappers/ruby-2.5.0@rails5/bundle'
+if current_env == 'production'
+  path_to_bundle = '/home/deploy/.rvm/wrappers/ruby-2.5.0@nsl-rails5/bundle'
+end
+
 # Alter cron job execution method depending on rails env.
-set :bundle_command, deployed ? '/home/deploy/.rvm/wrappers/ruby-2.5.0@rails5/bundle exec' : 'bundle exec'
+set :bundle_command, deployed ? "#{path_to_bundle} exec" : 'bundle exec'
 job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
 
 every 3.minutes do
