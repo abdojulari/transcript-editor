8d9b632b179ddaeba5b5ea25ae64354597918aba
Jason Corum <jason.corum@gmail.com>
2018-11-07 13:41:14 -0800

    Revert "Batches readers during transcript conversion"

--- lib/transcript_converter/runner.rb
+++ lib/transcript_converter/runner.rb
@@ -5,13 +5,13 @@ require 'transcript_converter/converter'
 module TranscriptConverter
   class Runner
 
-    attr_reader :transcript_files_batches, :to_format, :directory
+    attr_reader :transcript_files, :to_format, :directory
 
     def initialize(transcript_files_path, directory, to_format)
       raise "Directory doesn't exist at #{Rails.root + directory}" unless Dir.exist?(Rails.root + directory)
       @directory ||= Rails.root + directory
       @to_format ||= to_format
-      @transcript_files_batches = TranscriptConverter::TranscriptFiles.new(transcript_files_path).files.in_groups_of(100,false)
+      @transcript_files = TranscriptConverter::TranscriptFiles.new(transcript_files_path).files
     end
 
     def run!
@@ -23,7 +23,9 @@ module TranscriptConverter
     private
 
     def readers
-      @readers ||= process_transcript_files_batches
+      @readers ||= transcript_files.map do |file|
+        TranscriptConverter::Reader.factory(File.extname(File.basename(file.path)), file)
+      end
     end
 
     def converters
@@ -32,15 +34,5 @@ module TranscriptConverter
       end
     end
 
-    def process_transcript_files_batches
-      readers = []
-      transcript_files_batches.each do |batch|
-        require 'pry';binding.pry
-        batch.each do |file|
-          readers << TranscriptConverter::Reader.factory(File.extname(File.basename(file.path)), file)
-        end
-      end
-      readers
-    end
   end
 end
