f72636dd1b60146d2649083d0dcc97a4d2c6eef0
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2019-01-15 11:30:43 +1100

    [AM-108] Tell Capistrano to use updated ruby

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,7 @@ current_env = ENV['RAILS_ENV'] || 'development'
 deployed = ['staging', 'production'].include?(current_env)
 
 # Alter cron job execution method depending on rails env.
-set :bundle_command, deployed ? '/home/deploy/.rvm/wrappers/ruby-2.5.0@rails5/bundle exec' : 'bundle exec'
+set :bundle_command, deployed ? '/home/deploy/.rvm/wrappers/ruby-2.5.3@rails5/bundle exec' : 'bundle exec'
 job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
 
 every 3.minutes do
