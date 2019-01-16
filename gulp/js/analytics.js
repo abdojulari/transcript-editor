9df41e59fbb32209c760aac6be26bef65991dd2f
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2019-01-16 13:14:51 +1100

    [AM-106] Fix analytics js

--- gulp/js/analytics.js
+++ gulp/js/analytics.js
@@ -2,9 +2,8 @@
 (function() {
   window.ANALYTICS = {};
 
-  ANALYTICS.event = function(category, ev, label, value){
-    // console.log(category, ev, label, value)
-    if (ga) {
+  ANALYTICS.event = function(category, ev, label, value) {
+    if (typeof(ga) !== 'undefined') {
       if (label && value) ga('send', 'event', category, ev, label, value);
       else if (label) ga('send', 'event', category, ev, label);
       else ga('send', 'event', category, ev);
