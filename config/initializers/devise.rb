8a5827f2536e07437ac0c314fc26bd91a5396b34
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2019-04-29 11:15:03 +1000

    [AM-125] Upgrade Facebook Graph API version

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,8 +248,18 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true, provider_ignores_state: true
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  config.omniauth :google_oauth2,
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET'],
+    skip_jwt: true,
+    provider_ignores_state: true
+  config.omniauth :facebook,
+    ENV['FACEBOOK_APP_ID'],
+    ENV['FACEBOOK_APP_SECRET'],
+    client_options: {
+      site: 'https://graph.facebook.com/v3.2',
+      authorize_url: 'https://www.facebook.com/v3.2/dialog/oauth'
+    }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
