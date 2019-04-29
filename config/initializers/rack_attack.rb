b889ea66b9d8583d7845eb89f0e296f413cec5c7
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2019-04-29 12:21:02 +1000

    [AM-125] Documents initialiser, implements throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,14 @@
 # Initialise rack-attack to protect against brute forcing.
 
+# Whitelists any IPs that we know are OK.
 ra_whitelist = ""
 if ENV.key?("RACK_ATTACK_WHITELIST")
   ra_whitelist = ENV["RACK_ATTACK_WHITELIST"].to_s
 end
-
 ra_whitelist.split(/\s+/).reject(&:empty?).
   each { |ip| Rack::Attack.safelist_ip(ip) }
 
+# Blocks all requests from misbehaving clients.
 Rack::Attack.blocklist("fail2ban pentesters") do |req|
   Rack::Attack.Fail2Ban.filter(
     "pentesters-#{req.ip}",
@@ -18,12 +19,22 @@ Rack::Attack.blocklist("fail2ban pentesters") do |req|
     CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
       req.path.include?("/etc/passwd") ||
       req.path.include?("wp-admin") ||
-      req.path.include?("wp-login") ||
-      req.path.include?("/admin") ||
-      req.path.include?("/users")
+      req.path.include?("wp-login")
   end
 end
 
+# Throttles POST requests to /users pages by IP.
+Rack::Attack.throttle(
+  "limit users page attempts by ip",
+  limit: 10,
+  period: 60,
+) do |req|
+  if req.path.include?("/users") && req.post?
+    req.ip
+  end
+end
+
+# Provides a custom response to throttled clients.
 Rack::Attack.throttled_response = lambda do |env|
   match_data = env["rack.attack.match_data"]
   now = match_data[:epoch_time]
