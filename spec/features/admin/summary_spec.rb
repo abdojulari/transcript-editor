155bcf5ba023e8e6c208ff038b4cbcfe9950f198
Raluca Pintilii <ralucapintilii@gmail.com>
2019-06-11 18:38:42 +0100

    Added specs for the admin summary page

--- spec/features/admin/summary_spec.rb
+++ spec/features/admin/summary_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Summary Page' do
   let(:admin) { create(:user, :admin) }
 
-  describe 'as an admin', js: true do
-    context 'can see the Summary page' do
+  describe 'the Summary page as an admin', js: true do
+    context 'without collections and institutions' do
       before do
         sign_in admin
         visit admin_summary_index_path
@@ -13,6 +13,55 @@ RSpec.feature 'Summary Page' do
       it 'shows the summary page' do
         expect(page).to have_text('Summary of Completion percentages')
         expect(page).to have_current_path(admin_summary_index_path)
+      end
+
+      it 'shows an empty stats page' do
+        expect(page).to have_text('Total number of items: 0')
+        expect(page).to have_text('Total duration of items: 00h 00m 00s')
+        expect(page).to have_text('NaN %', count: 3)
+
+        expect(page).to have_text('Disk usage')
+        expect(page).to have_text('0 Bytes', count: 4)
+      end
+    end
+
+    context 'with collections and institutions' do
+      let!(:institution) { create(:institution) }
+      let!(:collection) { create(:collection, institution: institution) }
+      let!(:transcript) { create(:transcript, collection: collection, duration: 2000) }
+
+      let!(:another_institution) { create(:institution) }
+      let!(:another_collection) { create(:collection, institution: another_institution) }
+      let!(:another_transcript) { create(:transcript, collection: another_collection, duration: 1988) }
+
+      before do
+        sign_in admin
+        visit admin_summary_index_path
+      end
+
+      context 'viewing all stats' do
+        it 'shows all stats on the page' do
+          expect(page).to have_text('Total number of items: 2')
+          expect(page).to have_text('Total duration of items: 01h 06m 28s')
+          expect(page).to have_text('Completed')
+          expect(page).to have_text('0.00 %')
+        end
+      end
+
+      context 'sellecting an institution from the dropdown' do
+        it 'shows the stats of the first institution' do
+          select institution.name, from: 'institution_id'
+          expect(page).to have_text('Total number of items: 1')
+          expect(page).to have_text('Total duration of items: 00h 33m 20s')
+          expect(page).to have_text('Completed')
+          expect(page).to have_text('0.00 %')
+        end
+
+        it 'shows the stats of the second institution' do
+          select another_institution.name, from: 'institution_id'
+          expect(page).to have_text('Total number of items: 1')
+          expect(page).to have_text('Total duration of items: 00h 33m 08s')
+        end
       end
     end
   end
--- spec/helpers/admin/summary_helper_spec.rb
+++ spec/helpers/admin/summary_helper_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Admin::SummarydHelper, type: :helper do
+RSpec.describe Admin::SummaryHelper, type: :helper do
   describe '.prettify_duration' do
     it 'returns a humanized duration string from an integer' do
       expect(helper.prettify_duration(2000)).to eq('00h 33m 20s')
