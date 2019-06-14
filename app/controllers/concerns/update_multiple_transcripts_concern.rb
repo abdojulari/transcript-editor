2016726bb3f679d8a7b83906b85a7c04bbbd942a
Raluca Pintilii <ralucapintilii@gmail.com>
2019-06-14 01:14:48 +0100

    Changed bulk update syntax to find_each

--- app/controllers/concerns/update_multiple_transcripts_concern.rb
+++ app/controllers/concerns/update_multiple_transcripts_concern.rb
@@ -2,17 +2,17 @@ module UpdateMultipleTranscriptsConcern
   extend ActiveSupport::Concern
 
   def publish_trancripts(transcript_ids)
-    @collection.transcripts.where(uid: transcript_ids).each { |t| t.publish! }
+    @collection.transcripts.where(uid: transcript_ids).find_each(&:publish!)
     flash.now[:notice] = 'The selected transcripts were successfully published!'
   end
 
   def unpublish_trancripts(transcript_ids)
-    @collection.transcripts.where(uid: transcript_ids).each { |t| t.unpublish! }
+    @collection.transcripts.where(uid: transcript_ids).find_each(&:unpublish!)
     flash.now[:notice] = 'The selected transcripts were successfully unpublished!'
   end
 
   def delete_trancripts(transcript_ids)
-    @collection.transcripts.where(uid: transcript_ids).each { |t| t.destroy }
+    @collection.transcripts.where(uid: transcript_ids).find_each(&:destroy)
     flash.now[:notice] = 'The selected transcripts were successfully deleted!'
   end
 end
