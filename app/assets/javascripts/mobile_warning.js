d9eff8ce98e05439d4b08eeaded6ce16abb6744b
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-06-19 10:49:06 +1000

    refactoring the script

--- app/assets/javascripts/mobile_warning.js
+++ app/assets/javascripts/mobile_warning.js
@@ -2,26 +2,22 @@
 // mobile_warning.js
 
 $(document).ready(function() {
-  // Setting up two seperate dismissing scripts for each different pop=up type
+  // Setting up two seperate dismissing scripts for each different pop-up type
+  var homeWarning       = $('#mobile-warning[data-page="home"]');
+  var transcriptWarning = $('#mobile-warning[data-page="transcript"]');
 
-  var homeWarning = $('#mobile-warning[data-page="home"]')
-  var transcriptWarning = $('#mobile-warning[data-page="transcript"]')
+  var homeCookie        = 'Amplify.UI.MobileWarning.HomeDismissed';
+  var transcriptCookie  = 'Amplify.UI.MobileWarning.TranscriptDismissed'
 
-  var homeDismissed = window.localStorage.getItem('Amplify.UI.MobileWarning.HomeDismissed');
-  var transcriptDismissed = window.localStorage.getItem('Amplify.UI.MobileWarning.TranscriptDismissed');
+  var homeDismissed = window.localStorage.getItem(homeCookie);
+  var transcriptDismissed = window.localStorage.getItem(transcriptCookie);
 
   homeWarning.find('.button').click(function() {
-    window.localStorage.setItem('Amplify.UI.MobileWarning.HomeDismissed', '1');
-    setTimeout(function() {
-      homeWarning.attr('aria-hidden', 'true');
-    }, 0);
+    dismissCookie(homeWarning, homeCookie);
   });
 
   transcriptWarning.find('.button').click(function() {
-    window.localStorage.setItem('Amplify.UI.MobileWarning.TranscriptDismissed', '1');
-    setTimeout(function() {
-      transcriptWarning.attr('aria-hidden', 'true');
-    }, 0);
+    dismissCookie(transcriptWarning, transcriptCookie);
   });
 
   if ($(window).width() < 768 && !homeDismissed) {
@@ -31,3 +27,10 @@ $(document).ready(function() {
     transcriptWarning.removeAttr('aria-hidden');
   }
 });
+
+function dismissCookie(popUp, cookie) {
+  window.localStorage.setItem(cookie, '1');
+  setTimeout(function() {
+    popUp.attr('aria-hidden', 'true');
+  }, 0);
+}
