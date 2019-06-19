8c2f010a59e7e1001be0deb39b0d8c1f85287a0f
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-06-19 12:10:47 +1000

    rendering the span with the template instead of JS to improve performance

--- app/assets/javascripts/mobile_transcripts.js
+++ app/assets/javascripts/mobile_transcripts.js
@@ -1,41 +1,4 @@
 $(window).load(function(){
-  // Hides the line text input on mobile
-  // Replaces with static text
-  generateStaticText();
+  // Undisables the button after transcript is loaded
   $('.mobile-toggle').removeClass('disabled');
-
-  $(window).on('resize', function(){
-    toggleInput()
-  });
-
 });
-
-function generateStaticText() {
-  var $transcriptLineInputs = $('#transcript-lines .text-input')
-  $.each($transcriptLineInputs, function() {
-    var $input = $(this)
-    var $text  = $input.val()
-    var $container = $input.parent()
-
-    $container.append('<span class="mobile-text">' + $text + '</span>')
-  });
-  toggleInput()
-}
-
-function hideInput() {
-  $('#transcript-lines .text-input').hide();
-  $('#transcript-lines .mobile-text').show();
-}
-
-function showInput() {
-  $('#transcript-lines .text-input').show();
-  $('#transcript-lines .mobile-text').hide();
-}
-
-function toggleInput() {
-  if ($(window).width() < 768) {
-    hideInput();
-  } else {
-    showInput();
-  }
-}
