1eba39f45ded4b4c0aea1bbd77e749902d09dedf
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-06-19 10:26:54 +1000

    making the seperate pop-ups separately dismissible

--- app/assets/javascripts/mobile_warning.js
+++ app/assets/javascripts/mobile_warning.js
@@ -2,16 +2,32 @@
 // mobile_warning.js
 
 $(document).ready(function() {
-  var dismissed = window.localStorage.getItem('Amplify.UI.MobileWarning.Dismissed');
+  // Setting up two seperate dismissing scripts for each different pop=up type
 
-  $('#mobile-warning button').click(function() {
-    window.localStorage.setItem('Amplify.UI.MobileWarning.Dismissed', '1');
+  var homeWarning = $('#mobile-warning[data-page="home"]')
+  var transcriptWarning = $('#mobile-warning[data-page="transcript"]')
+
+  var homeDismissed = window.localStorage.getItem('Amplify.UI.MobileWarning.HomeDismissed');
+  var transcriptDismissed = window.localStorage.getItem('Amplify.UI.MobileWarning.TranscriptDismissed');
+
+  homeWarning.find('.button').click(function() {
+    window.localStorage.setItem('Amplify.UI.MobileWarning.HomeDismissed', '1');
     setTimeout(function() {
-      $('#mobile-warning').attr('aria-hidden', 'true');
+      homeWarning.attr('aria-hidden', 'true');
     }, 0);
   });
 
-  if ($(window).width() < 768 && !dismissed) {
-    $('#mobile-warning').removeAttr('aria-hidden');
+  transcriptWarning.find('.button').click(function() {
+    window.localStorage.setItem('Amplify.UI.MobileWarning.TranscriptDismissed', '1');
+    setTimeout(function() {
+      transcriptWarning.attr('aria-hidden', 'true');
+    }, 0);
+  });
+
+  if ($(window).width() < 768 && !homeDismissed) {
+    homeWarning.removeAttr('aria-hidden');
+  }
+  if ($(window).width() < 768 && !transcriptDismissed) {
+    transcriptWarning.removeAttr('aria-hidden');
   }
 });
