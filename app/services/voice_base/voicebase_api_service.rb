324854ac096d03c0658bbc8ce8ac08047be7e600
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2020-03-09 13:30:54 +1100

    [AM-137] Refine Voicebase processing.
    * Don't do check_progress unless we've got a valid voicebase_media_id.
    * Queue transcripts for re-upload if necessary.

--- app/services/voice_base/voicebase_api_service.rb
+++ app/services/voice_base/voicebase_api_service.rb
@@ -2,41 +2,58 @@ module VoiceBase
   class VoicebaseApiService
     attr_accessor :transcript
 
+    # Upload audio file to Voicebase for processing.
     def self.upload_media(transcript_id)
       transcript = Transcript.find(transcript_id)
 
       #NOTE: this code is to test the functionality locally
       #      otherwise, since the file is not uploading to aws
       #      VoiceBase upload will not work as it cannot access
-      #      the image url
+      #      the image url.
       if Rails.env.development?
         res = Voicebase::Client.new.upload_media("https://slnsw-amplify.s3.amazonaws.com/collections_v2/snowymountain_bushfires/audio/mloh307-0001-0004-s002-m.mp3")
       else
         res = Voicebase::Client.new.upload_media(transcript.audio_url)
       end
       res = Voicebase::Client.new.upload_media(transcript.audio_url)
-      status = JSON.parse res.body
+      status = JSON.parse(res.body)
       if status["errors"]
-        Bugsnag.notify("Voicebase errors for transcript #{transcript_id} :  #{status["errors"]}")
+        Bugsnag.notify("Could not do Voicebase audio upload for transcript #{transcript_id}: #{status["errors"]}")
       else
         transcript.update_column("voicebase_media_id", status["mediaId"])
       end
     end
 
+    # Check progress of Voicebase processing.
     def self.check_progress(transcript_id)
       transcript = Transcript.find(transcript_id)
-      # mark the record as processing, so that other processors will not
-      # pick it up
+
+      # If it hasn't been uploaded to Voicebase yet,
+      # better do it first.
+      unless transcript.voicebase_media_id
+        if transcript.audio_url
+          Bugsnag.notify("Could not do Voicebase processing for transcript #{transcript_id}: Audio content not uploaded yet, rescheduling.")
+          VoiceBaseUploadJob.perform_later(transcript_id)
+        else
+          Bugsnag.notify("Could not do Voicebase processing for transcript #{transcript_id}: No audio content available for processing.")
+        end
+        return false
+      end
+
+      # Mark the record as processing, so that other processors
+      # will not pick it up.
       transcript.update_column("pickedup_for_voicebase_processing_at", Time.zone.now)
       res = Voicebase::Client.new.check_progress(transcript.voicebase_media_id)
-      status = JSON.parse res.body
+      status = JSON.parse(res.body)
       if status["errors"]
-        Bugsnag.notify("Voicebase errors for transcript #{transcript_id} :  #{status["errors"]}")
+        Bugsnag.notify("Voicebase processing errors for transcript #{transcript_id}: #{status["errors"]}")
+        return false
       else
         transcript.update_column("voicebase_status", status["progress"]["status"])
       end
     end
 
+    # Download completed transcript from Voicebase, and import.
     def self.process_transcript(transcript_id)
       transcript = Transcript.find(transcript_id)
 
@@ -54,6 +71,7 @@ module VoiceBase
       transcript.update_column("pickedup_for_voicebase_processing_at", nil)
     end
 
+    # Download completed transcript from Voicebase.
     def self.get_transcript(transcript_id)
       transcript = Transcript.find(transcript_id)
       unless transcript.voicebase_media_id
