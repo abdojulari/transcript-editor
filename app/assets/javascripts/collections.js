8e1d7485a936e084becb820df0a19d9b79955981
Estevão de Abreu Machado <estevao@reinteractive.net>
2020-08-20 15:17:13 +1000

    Fix collections page (#114)

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  var institutionId = 0;
+
+  $('#data_institution_id').on('change', function(){
+    institutionId = $(this).val();
+    loadCollections();
+  });
+
+  function loadCollections(){
+    data = {
+      institution_id: institutionId,
+    };
+    $(".collect-list").html('<div class="lds-ripple"><div></div><div></div></div>')
+    $.ajax({
+      type: "POST",
+      url: "/collections/list",
+      data: {institution_id: institutionId},
+      error: function(jqXHR, textStatus, errorThrown){
+        $(".collect-list").html('Error when reading collection..')
+      }
+    })
+  }
+  loadCollections()
+})
