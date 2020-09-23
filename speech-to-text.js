5086eb9b635ed2be189f8c0af98fb414d8bdcdc9
Tian Chen <me@tian.im>
2020-09-23 17:55:23 +1000

    feat: add speech-to-text.js

---
+++ speech-to-text.js
@@ -0,0 +1,90 @@
+// NOTE: the microsoft speech-to-text nodejs SDK only supports WAV file in the following format:
+// Property	Value
+// File format	RIFF(WAV)
+// Sample rate	8, 000 Hz or 16, 000 Hz
+// Channels	1(mono)
+// Maximum length per audio	2 hours
+// Sample format	PCM, 16 - bit
+// Archive format .zip
+// Maximum archive size	2 GB
+// @see https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-custom-speech-test-and-train#audio-data-for-testing
+(function () {
+  "use strict";
+
+  try { require('dotenv').config(); } catch (err) {};
+  const sdk = require("microsoft-cognitiveservices-speech-sdk");
+  const fs = require("fs");
+
+  const filename = process.argv[2];
+  if (!filename) {
+    exitLog(`
+      usage: node speech-to-text.js <path_to_wav_file> [<language>]
+
+      <path_to_wav_file>: required. it's the path to the wav file to be transcribed.
+      <language>: optional. e.g. en-US
+    `);
+  }
+  const recognizer = recognizerFor(streamAudio(filename));
+  setup(recognizer);
+  recognizer.startContinuousRecognitionAsync();
+
+  function streamAudio(filename) {
+    const pushStream = sdk.AudioInputStream.createPushStream();
+
+    fs.createReadStream(filename)
+      .on('data', function (arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+      }).on('end', function () {
+        pushStream.close();
+      }).on('error', function (error) {
+        exitLog(error.message);
+      });
+
+    return pushStream;
+  }
+
+  function recognizerFor(pushStream) {
+    const subscriptionKey = process.env.SPEECH_TO_TEXT_KEY;
+    const serviceRegion = process.env.SPEECH_TO_TEXT_REGION;
+
+    const audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+    const speechConfig = sdk.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+    if (process.argv[3]) { speechConfig.speechRecognitionLanguage = process.argv[3] ; }
+    speechConfig.enableDictation();
+
+    return new sdk.SpeechRecognizer(speechConfig, audioConfig);
+  }
+
+  function setup(recognizer) {
+    const lines = [];
+
+    recognizer.recognized = (s, e) => {
+      // NOTE: we only capture the recognized text. everything else will be ignored.
+      if (e.result.reason === sdk.ResultReason.RecognizedSpeech) {
+        lines.push({
+          start_time: e.result.offset,
+          end_time: e.result.offset + e.result.duration,
+          original_text: e.result.text,
+          sequence: lines.length
+        })
+      }
+    };
+
+    recognizer.canceled = (s, e) => {
+      recognizer.stopContinuousRecognitionAsync();
+
+      if (e.reason === sdk.CancellationReason.Error) {
+        exitLog(`${e.errorCode}: ${e.errorDetails}`);
+      } else {
+        // it ends without any error
+        console.log(JSON.stringify(lines));
+        process.exit();
+      }
+    };
+  }
+
+  function exitLog(message, exitCode) {
+    console.log(message);
+    process.exit(exitCode || 1);
+  }
+}());
