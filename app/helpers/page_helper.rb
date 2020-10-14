34d2a442c9feefda6ddc95040bc2a7bdacb7005f
Estevão de Abreu Machado <estevao@reinteractive.net>
2020-10-14 12:08:41 +1000

    Split homepage and search page forms

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -7,7 +7,7 @@ module PageHelper
 
   def collection_checked?(box)
     return unless params.dig(:data, :collection_id).is_a? Array
-    binding.pry
+
     params.dig(:data, :collection_id).include?(box.object.id.to_s)
   end
 end
