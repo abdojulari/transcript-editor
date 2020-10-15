7d5a586e9c95a798d5a1e330008664d1471e08bc
Estevão de Abreu Machado <estevao@reinteractive.net>
2020-10-15 11:29:13 +1000

    Fix search page filters

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -11,9 +11,15 @@ module PageHelper
     @build_params[:collections].include?(box.object.title.to_s)
   end
 
-  def collection_id_checked?(box)
-    return unless @build_params[:collection_id].is_a? Array
+  def search_themes_checked?(box)
+    return unless params.dig(:data, :theme).is_a? Array
 
-    @build_params[:collection_id].include?(box.object.title.to_s)
+    params.dig(:data, :theme).include?(box.object.name.to_s)
+  end
+
+  def search_collection_checked?(box)
+    return unless params.dig(:data, :collection_id).is_a? Array
+
+    params.dig(:data, :collection_id).include?(box.object.id.to_s)
   end
 end
