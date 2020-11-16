0ef4045d511e065cbd74e3b32e9c384bf19bdaee
Estevão de Abreu Machado <estevao@reinteractive.net>
2020-11-16 14:38:11 +1000

    Fix tests

--- spec/factories/institution.rb
+++ spec/factories/institution.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :institution do
     name { Faker::University.name }
     url { 'http://google.com' }
-    slug { 'wagga-wagga' }
+    slug { Faker::Lorem.characters(10) }
   end
 end
