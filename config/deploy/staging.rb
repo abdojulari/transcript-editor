abbd7b35ec82a05009bb7e264a89d23afaa5a786
Rodrigo Assis <1258016+rodrigoassis@users.noreply.github.com>
2021-06-15 21:23:02 -0300

    adding basic spec to guarantee not logged users dont break (#165)
    * adding basic spec to guarantee not logged users dont break
    
    * allowing deploy from any branch to staging (#166)
    
    * allowing deploy from any branch to staging
    
    * branch env var to deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,7 @@
 server 'ec2-52-63-16-71.ap-southeast-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
 set :rails_env, 'staging'
-set :branch, 'develop'
-
+set :branch, ENV['BRANCH'] || 'develop'
 
 # server-based syntax
 # ======================
@@ -12,8 +11,6 @@ set :branch, 'develop'
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
-
 
 # role-based syntax
 # ==================
@@ -27,8 +24,6 @@ set :branch, 'develop'
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -36,8 +31,6 @@ set :branch, 'develop'
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
-
 
 # Custom SSH Options
 # ==================
