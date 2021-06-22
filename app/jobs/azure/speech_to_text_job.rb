b2e4de4aeb93bb4056964c43171e606798a1535c
Rodrigo Assis <1258016+rodrigoassis@users.noreply.github.com>
2021-06-21 23:17:30 -0300

    Feature/keep wav file (#78)
    * adding basic spec to guarantee not logged users dont break
    
    * allowing deploy from any branch to staging
    
    * branch env var to deploy
    
    * keeping wav file after azure convert
    
    * onlt clear transcript lines if there are no lines
    
    * allowing deploy to production from other branches

--- app/jobs/azure/speech_to_text_job.rb
+++ app/jobs/azure/speech_to_text_job.rb
@@ -16,17 +16,25 @@ module Azure
         process_message: nil,
         process_started_at: Time.current
       )
-      lines = Azure::SpeechToTextService.new(file: file).recognize
-      ActiveRecord::Base.transaction do
-        transcript.transcript_lines.clear
-        lines.each do |line_attrbitues|
-          transcript.transcript_lines.create line_attrbitues
+
+      speech_to_text = Azure::SpeechToTextService.new(file: file).recognize
+      lines = speech_to_text.lines
+      wav_file = File.open(speech_to_text.wav_file_path)
+
+      if transcript.transcript_lines.count == 0
+        ActiveRecord::Base.transaction do
+          transcript.transcript_lines.clear
+          lines.each do |line_attrbitues|
+            transcript.transcript_lines.create(line_attrbitues)
+          end
+          transcript.update_columns(
+            lines: lines.length,
+            duration: `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 #{file.to_s}`.to_i,
+          )
         end
-        transcript.update_columns(
-          lines: lines.length,
-          duration: `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 #{file.to_s}`.to_i,
-        )
       end
+
+      transcript.update(audio: wav_file)
       transcript.update_columns(
         process_status: :completed,
         process_message: nil,
@@ -40,6 +48,7 @@ module Azure
       Bugsnag.notify e
     ensure
       File.delete file if file && File.exist?(file)
+      File.delete speech_to_text&.wav_file_path if File.exist?(speech_to_text&.wav_file_path.to_s)
     end
 
     def download(audio)
