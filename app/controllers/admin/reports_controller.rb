b2cdecaa64e43a42c109c76cf405b682e1a5d8b2
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2023-09-04 16:50:13 +1000

    [AM-195] CSV export

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Admin::ReportsController < AdminController
   before_action :authenticate_admin!
   before_action :get_date_params
@@ -19,6 +21,13 @@ class Admin::ReportsController < AdminController
   def users
     params[:page] ||= 1
     @users = User.getReport(page: params[:page], per_page: 20)
+
+    respond_to do |wants|
+      wants.html
+      wants.csv do
+        render_csv("users-#{Time.now.strftime("%Y%m%d-%H%M%S")}")
+      end
+    end
   end
 
   private
@@ -31,6 +40,24 @@ class Admin::ReportsController < AdminController
       # look for parameters
       @start_date = params[:start].to_datetime unless params[:start].blank?
       @end_date = params[:end].to_datetime unless params[:end].blank?
+    end
+
+    def render_csv(filename = nil)
+      filename ||= params[:action]
+      filename += '.csv'
+    
+      if request.env['HTTP_USER_AGENT'] =~ /msie/i
+        headers['Pragma'] = 'public'
+        headers["Content-type"] = "text/plain" 
+        headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+        headers['Content-Disposition'] = "attachment; filename=\"#{filename}\"" 
+        headers['Expires'] = "0" 
+      else
+        headers["Content-Type"] ||= 'text/csv'
+        headers["Content-Disposition"] = "attachment; filename=\"#{filename}\"" 
+      end
+    
+      render :layout => false
     end
 
 end
